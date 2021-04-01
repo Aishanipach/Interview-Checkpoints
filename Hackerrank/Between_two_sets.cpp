34b5620d9b7c4aaf91c47482ac20120b1b5b5c4c
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-04-01 20:34:17 +0530

    Create Between_two_sets.cpp

---
+++ Hackerrank/Between_two_sets.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+int getTotalX(vector<int> a, vector<int> b) {
+
+    int size_a= a.size();
+    int size_b= b.size();
+    int ans=0;
+    for (int i=1;i<=100;i++)
+    {   
+        int flag=1;
+        
+        for(int j=0;j<size_a;j++)
+            if(i%a[j]!=0)
+                flag=0;
+                
+        for(int k=0;k<size_b;k++)
+            if(b[k]%i!=0)
+                flag=0;
+        if(flag==1)
+            ans++;
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    string brr_temp_temp;
+    getline(cin, brr_temp_temp);
+
+    vector<string> brr_temp = split(rtrim(brr_temp_temp));
+
+    vector<int> brr(m);
+
+    for (int i = 0; i < m; i++) {
+        int brr_item = stoi(brr_temp[i]);
+
+        brr[i] = brr_item;
+    }
+
+    int total = getTotalX(arr, brr);
+
+    fout << total << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
