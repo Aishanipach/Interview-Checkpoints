51fc6f0d59895567f963ad6aa0b140eb20cc5770
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-06-02 23:41:57 +0530

    Update Designer_PDF_Viewer.cpp

--- Hackerrank/Designer_PDF_Viewer.cpp
+++ Hackerrank/Designer_PDF_Viewer.cpp
@@ -17,6 +17,21 @@ vector<string> split(const string &);
 
 int designerPdfViewer(vector<int> h, string word) {
 
+map<char, int> alpha {{'a',0},{'b',1},{'c',2},{'d',3},{'e',4},{'f',5},{'g',6},{'h',7},{'i',8},{'j',9},{'k',10},{'l',11},{'m',12},{'n',13},{'o',14},{'p',15},{'q',16},{'r',17},{'s',18},{'t',19},{'u',20},{'v',21},{'w',22},{'x',23},{'y',24},{'z',25}};
+
+int max=0;
+ map<char, int>::iterator it1;
+for(auto &it:word)
+{
+    
+    it1=alpha.find(it);
+    if(h[it1->second]>max)
+        max=h[it1->second];
+          
+}
+
+return max*(word.length());
+
 }
 
 int main()
