a63d51ee950bee2dd373c5433787f75cb64e0801
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-06-02 23:43:07 +0530

    Add files via upload

---
+++ Hackerrank/Designer_PDF_Viewer.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+int designerPdfViewer(vector<int> h, string word) {
+
+map<char, int> alpha {{'a',0},{'b',1},{'c',2},{'d',3},{'e',4},{'f',5},{'g',6},{'h',7},{'i',8},{'j',9},{'k',10},{'l',11},{'m',12},{'n',13},{'o',14},{'p',15},{'q',16},{'r',17},{'s',18},{'t',19},{'u',20},{'v',21},{'w',22},{'x',23},{'y',24},{'z',25}};
+
+int max=0;
+ map<char, int>::iterator it1;
+for(auto &it:word)
+{
+    
+    it1=alpha.find(it);
+    if(h[it1->second]>max)
+        max=h[it1->second];
+          
+}
+
+return max*(word.length());
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split(rtrim(h_temp_temp));
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
