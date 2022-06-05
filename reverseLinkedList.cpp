9bfdabbce0dc21406b39ed1a22721a541f826a8f
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-06-05 12:51:52 +0530

    Create reverseLinkedList.cpp

---
+++ reverseLinkedList.cpp
@@ -0,0 +1,14 @@
+// Recursive
+
+ListNode* reverseList(ListNode* head) {
+        
+   if (head == NULL || head->next == NULL) return head;
+        
+        ListNode* newHead  = reverseList(head->next);
+        
+        head->next->next = head;
+        head->next = NULL;
+        
+        return newHead ;
+    }
+
