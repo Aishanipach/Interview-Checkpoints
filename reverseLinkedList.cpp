c10ec962a7962daa317c41855c0daa302641acce
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-06-05 14:28:14 +0530

    Update reverseLinkedList.cpp

--- reverseLinkedList.cpp
+++ reverseLinkedList.cpp
@@ -12,3 +12,36 @@ ListNode* reverseList(ListNode* head) {
         return newHead ;
     }
 
+
+//O(N)
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+     ListNode* reverseList(ListNode* head) {
+   ListNode *tmp, *rev = NULL;
+        
+        while(head){
+           
+              tmp = head->next;
+            
+              head->next = rev;
+              rev = head;
+              head = tmp;
+        }
+              
+        return rev;
+    }
+	
+
+};
