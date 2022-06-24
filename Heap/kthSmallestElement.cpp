3356c5401d46246b917c390dc0519a57f5b44b89
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-06-24 19:53:40 +0530

    Update kthSmallestElement.cpp

--- Heap/kthSmallestElement.cpp
+++ Heap/kthSmallestElement.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+  int kthSmallest(vector<int> arr,k){
+    
+    priority_queue <int> maxheap;
+    
+    for(int i=0;i<n;i++)
+    {
+      maxheap.push(arr[i]);
+      
+      if(maxheap.size()>k)
+        maxheap.pop();
+      
+    }
+    
+    return maxheap.top();
+              
+  }
+                
+               
+};
