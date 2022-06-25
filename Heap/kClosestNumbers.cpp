0167d90b808711adee4f947ee4c6a8592946347f
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-06-25 13:20:29 +0530

    Create kClosestNumbers.cpp

---
+++ Heap/kClosestNumbers.cpp
@@ -0,0 +1,17 @@
+//We need k numbers that have least difference from the given number, i.e. x, so we use maxheap
+
+class Solution{
+  public:
+  
+  void kClosestNumbers( vector<int> arr, int k, int x){
+    priority_queue <pair<int, int>> maxheap;
+    
+    for(int i=0;i<n;i++){
+       maxheap.push(make_pair(abs(arr[i]-x), arr[i]));
+       if(maxheap.size()>k)
+        maxheap.pop();
+    }
+    
+   
+  }
+};
