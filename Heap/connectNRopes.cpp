b4b2425e67d843f6d9d215a03b240394ff6b761b
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-06-25 13:51:59 +0530

    Create connectNRopes.cpp

---
+++ Heap/connectNRopes.cpp
@@ -0,0 +1,29 @@
+//We need min value at top for optimum ans, so min heap
+
+
+class Solution{
+  public:
+  
+  void connectNRopes(vector<int> arr){
+    
+    priority_queue< int, vector<int>, greater<int>> minheap;
+    int cost=0;
+    for(int i=0;i<n;i++)
+    {
+      minheap.push(arr[i]);
+    }
+    
+    while(minheap.size()>2){
+      int first= minheap.top();
+      minheap.pop();
+      
+      int second=minheap.top();
+      minheap.pop();
+      
+      cost+=first+second;
+      minheap.push(cost);
+    }
+    
+    cout<<cost;
+  }
+};
