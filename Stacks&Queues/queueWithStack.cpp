17e605d0dfa21a4b8abadf6b64addacc97f4d8e4
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-06-26 23:17:10 +0530

    Create queueWithStack.cpp

---
+++ Stacks&Queues/queueWithStack.cpp
@@ -0,0 +1,43 @@
+class MyQueue {
+public:
+    
+    stack < int > input, output;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+       input.push(x);
+    }
+    
+    int pop() {
+        if (output.empty())
+      while (input.size())
+        output.push(input.top()), input.pop();
+
+    int x = output.top();
+    output.pop();
+    return x;
+    }
+    
+    int peek() {
+      if (output.empty())
+      while (input.size())
+        output.push(input.top()), input.pop();
+    return output.top();
+        
+    }
+    
+    bool empty() {
+        return (!(output.size() + input.size())); 
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
