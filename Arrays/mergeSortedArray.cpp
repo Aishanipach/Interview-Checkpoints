7a48851bde0021a3769df1f43428d5d51ae4d8b1
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-07-08 18:35:08 +0530

    Update mergeSortedArray.cpp

--- Arrays/mergeSortedArray.cpp
+++ Arrays/mergeSortedArray.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=n;
-        int k=0;
-        while(i!=0){
-         
-            nums1.at(m+k)=*(nums2.begin()+k);
-            i--;
-            k++;
+        int i=0;
+        int gap=ceil(float)(n+m)/2;
+        int j=gap;
+
+        while(gap>0){
+          while(j<m+n){
+            if(j<m && nums1[j]<nums1[1])
+               swap(nums1[j],nums1[i]);
+            else if(j>=m && i<m && nums2[j-m]<nums1[i])
+               swap(nums2[j-m], nums1[i]);
+            else if(j>=m && i>m && nums2[j-m]<nums2[i-n])
+               swap(nums2[j-m], nums2[i-n]);
+
+            i++; j++;
+          }
+          if(gap==1)
+            gap=0;
+          else
+            gap=ceil(gap/2);
+
         }
-        
-        sort(nums1.begin(), nums1.end());
     }
 };
