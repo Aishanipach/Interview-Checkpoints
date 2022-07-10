9ead63daf62657eff9a1de7565bd0bb9e97732f1
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-07-10 15:27:58 +0530

    Create leftViewofTree.cpp

---
+++ BinaryTree/leftViewofTree.cpp
@@ -0,0 +1,19 @@
+//if the node we are at on this level is the first that we have encountered. ans.push_back()
+
+
+class Solution {
+public:
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL) return ;
+        if(res.size()==level) res.push_back(root->val);
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+    }
+    
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
+    }
+};
