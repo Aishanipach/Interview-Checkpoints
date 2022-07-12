fd9af1c7e62243ffc14003c905d91353474a1a1f
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-07-12 14:16:57 +0530

    Create maximumWidth.cpp

---
+++ BinaryTree/maximumWidth.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        
+        if(root==NULL)
+            return 0;
+        
+        long ans=0;
+        queue<pair<TreeNode*, long int>> q;
+        
+        q.push({root,0});
+        
+        while(!q.empty()){
+            
+            int size=q.size();
+            int min=q.front().second;
+            long first, last;
+            for(int i=0;i<size;i++)
+            {
+                long cur=q.front().second-min;
+                TreeNode* node= q.front().first;
+                q.pop();
+                if(i==0) first=cur;
+                if(i==size-1) last=cur;
+                if(node->left)
+                    q.push({node->left, cur*2+1});
+                
+                if(node->right)
+                    q.push({node->right, cur*2+2});
+                            
+                    
+                
+            }
+            
+            ans=ans>last-first+1?ans:last-first+1;
+            
+            
+        }
+        
+        return ans;
+    }
+};
