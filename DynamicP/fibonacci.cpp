440086c18b28ef6f134b6e4563a4ba5e8ec4872f
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-07-30 23:58:41 +0530

    Create fibonacci.cpp

---
+++ DynamicP/fibonacci.cpp
@@ -0,0 +1,15 @@
+
+class Solution:
+{
+  public:
+  
+  void fibo(int n, vector<int> &dp){
+    
+  }
+  
+  vector<int> solve(n){
+    vector<int> dp(n,-1);
+    
+    fibo(n, dp);
+    return dp;
+  }
