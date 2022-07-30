6821e71884aeb2da3577485cca5804b91315b4e7
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-07-31 00:00:47 +0530

    Update fibonacci.cpp

--- DynamicP/fibonacci.cpp
+++ DynamicP/fibonacci.cpp
@@ -5,6 +5,16 @@ class Solution:
   
   void fibo(int n, vector<int> &dp){
     
+    if(n<=1)
+      return n;
+    
+    if(dp[n]!=-1)
+      return dp[n];
+    
+    return dp[n]=fibo(n-1, dp)+fibo(n-2, dp);
+    
+    
+    
   }
   
   vector<int> solve(n){
