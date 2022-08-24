e570f5a5b3a91b697a7fb08156e115d91e2ee9da
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-08-24 04:00:18 -0700

    Update divideTwoIntergers.cpp

--- bitManipulation/divideTwoIntergers.cpp
+++ bitManipulation/divideTwoIntergers.cpp
@@ -1,1 +1,28 @@
-
+long long divide(long long dividend, long long divisor) 
+    {
+        int sign=1;
+        
+        if(dividend<0 ^ divisor<0)
+            sign=-1;
+        
+        
+        divisor=(abs(divisor));
+        dividend= abs(dividend);
+        long long quot=0;
+        long long temp=0;
+        
+        for(int i=31;i>=0;i--){
+            
+            if(temp+(divisor<<i)<=dividend){
+                temp+=divisor<<i;
+        
+                quot|=1LL<<i;
+            }
+        }
+        
+        if(sign==-1)
+            quot=-quot;
+            
+        return quot;
+            
+    }
