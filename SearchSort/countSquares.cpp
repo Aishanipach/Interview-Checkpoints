10222f14bcb77ac2f26af9ece48faf0434801156
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2022-09-19 14:19:38 +0530

    Update countSquares.cpp

--- SearchSort/countSquares.cpp
+++ SearchSort/countSquares.cpp
@@ -1,26 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
+  public:
+    int countSquares(int N) {
         
+        int n=sqrt(N);
+        if(N==0 || N==1)
+            return 0;
         
-        int n=target;
-        int i;
-        for(i=0;i<nums.size();i++){
-            if(nums[i]==n)
-                return i;
+        int count=1;
+        
+        while(n>1){
+            if(n*n<N){
+                count++;
+                n--;
+                
+            }
             
-            else if(n>nums[i])
-                break;
+            else
+                n--;
         }
         
-        for(int j=nums.size()-1;j>=i;j--)
-        {
-            if(nums[j]==n)
-                return j;
-            
-        }
-        
-        return -1;
-        
+       return count;
     }
 };
