aac95d1146fee1d32de915584c14c42df4ca05c1
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2024-06-09 04:03:15 +0530

    Create mergeKLinkedLists.cpp

---
+++ LinkedList/mergeKLinkedLists.cpp
@@ -0,0 +1,45 @@
+//Most optimised solution: min_heap
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        // create min_heap out of priority_queue 
+        //to store minimum value on top
+        priority_queue <pair<int, ListNode *>, 
+        vector<pair<int, ListNode *>>, 
+        greater<pair<int, ListNode *>>> pq;
+
+        for(int i =0;i<lists.size();i++){
+            if(lists[i])
+            pq.push({lists[i]->val,lists[i]});
+
+        }
+
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(!pq.empty()){
+            auto it= pq.top();
+            pq.pop();
+            if(it.second->next){
+                pq.push({it.second->next->val, it.second-> next});
+            }
+            
+            temp->next = it.second;
+            temp = temp->next;
+        }
+
+    return dummyNode->next;
+    }
+};
