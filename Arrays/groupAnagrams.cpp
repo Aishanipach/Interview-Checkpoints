3d6c68bd70ef72952428f33aec1755111d902c69
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2025-01-13 17:14:52 +0530

    Update groupAnagrams.cpp

--- Arrays/groupAnagrams.cpp
+++ Arrays/groupAnagrams.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
 
+        for(int i=0;i<strs.size();i++){
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            mp[key].push_back(strs[i]);
+        }
+        vector<vector<string>> result;
+           for(auto &entry: mp){
+            result.push_back(entry.second);
+            }
+
+return result;
+    }
+};
