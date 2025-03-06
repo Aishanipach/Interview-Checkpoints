6040a2dcfb26dc24a8bb6cb4c0a2ba826d53c409
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2025-03-06 10:02:21 +0530

    Update intro.cpp

--- Graph/intro.cpp
+++ Graph/intro.cpp
@@ -1,16 +1,38 @@
+// O(2*Edges)
 #include <iostream>
 using namespace std;
 
 int main(){
 
   int n, m;
-  cin>>n>>m;
-  int adj[n+1][m+1];
-  for(int i =0;i<m;i++){
-      int u, v;
-      cin>>u>>v;
-      adj[u][v]=1;
-      adj[v][u]=1;
-  }
-  return 0;
+  cin>>n, m;
+  vector<int>adj[n+1];
+  int(int i=0;i<m;i++){
+    int u, v;
+    
 }
+
+
+
+
+
+
+
+
+// O(n^2)
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//   int n, m;
+//   cin>>n>>m;
+//   int adj[n+1][m+1];
+//   for(int i =0;i<m;i++){
+//       int u, v;
+//       cin>>u>>v;
+//       adj[u][v]=1;
+//       adj[v][u]=1;
+//   }
+//   return 0;
+// }
